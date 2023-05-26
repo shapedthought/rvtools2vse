3aba124f1705e85ae4548d5a0f5621949079f4b3
Edward Howard <edward.x.howard@veeam.com>
2023-05-26 09:21:00 +0100

    Traits working

--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -26,24 +26,6 @@ impl ColPosition for Range<DataType> {
     }
 }
 
-pub fn get_col_position(range: &Range<DataType>, col_name: &String) -> Result<usize, MyError> {
-    let data_type = range.rows().next().unwrap();
-
-    let pos = data_type
-        .iter()
-        .position(|x| x == &DataType::String(col_name.to_string()));
-
-    if let Some(p) = pos {
-        Ok(p)
-    } else {
-        Err(MyError::ColumnPosition(format!(
-            "{} - {:?}",
-            col_name.to_string(),
-            data_type
-        )))
-    }
-}
-
 pub trait GetString {
     fn get_string_value(&self, item: String, row: usize) -> Result<String, MyError>;
 }
@@ -58,17 +40,6 @@ impl GetString for DataType {
             item, row, self
         ))),
     }
-    }
-}
-
-pub fn get_string_value(data_type: &DataType, item: String, row: usize) -> Result<String, MyError> {
-    match data_type {
-        DataType::String(t) => Ok(t.to_string()),
-        DataType::Empty => Ok("None".to_string()),
-        _ => Err(MyError::EnumToString(format!(
-            "{} - row {} - Datatype {:?}",
-            item, row, data_type
-        ))),
     }
 }
 
@@ -90,14 +61,3 @@ impl GetFloat for DataType {
     }
 }
 
-pub fn get_float_value(data_type: &DataType, item: String, row: usize) -> Result<f64, MyError> {
-    match data_type {
-        DataType::Float(t) => Ok(*t),
-        DataType::Int(t) => Ok(*t as f64),
-        DataType::Empty => Ok(0.0),
-        _ => Err(MyError::EnumToFloat(format!(
-            "{} - row {} - Datatype {:?}",
-            item, row, data_type
-        ))),
-    }
-}
